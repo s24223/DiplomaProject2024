779338892a05af2ab33f2b8f61db4c7de2927804
s24610 <s24610@pjwstk.edu.pl>
2024-10-28 13:26:32 +0100

    front-end fix

--- front-end-react/src/pages/MainPage/MainPage.js
+++ front-end-react/src/pages/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-//import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import LoginButton from '../../components/LoginButton/LoginButton';
 import { fetchOffers } from '../../services/OffersService/OffersService';
@@ -6,27 +6,27 @@ import OffersList from '../../components/OffersList/OffersList';
 import './MainPage.css';
 
 const MainPage = () => {
-    // const [offers, setOffers] = useState([]);
-    // const [searchQuery, setSearchQuery] = useState('');
+    const [offers, setOffers] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
-    // useEffect(() => {
-    //     const loadOffers = async () => {
-    //         try {
-    //             const data = await fetchOffers();//searchQuery);
-    //             setOffers(data);
-    //         } catch (error) {
-    //             console.error("Error fetching offers:", error);
-    //         }
-    //     };
-    //     loadOffers();
-    // }, [searchQuery]);
+    useEffect(() => {
+        const loadOffers = async () => {
+            try {
+                const data = await fetchOffers(searchQuery);
+                setOffers(data);
+            } catch (error) {
+                console.error("Error fetching offers:", error);
+            }
+        };
+        loadOffers();
+    }, [searchQuery]);
 
     return (
         <div className="main-page">
             <h1>Strona główna</h1>
-            {/* <LoginButton />
+            <LoginButton />
             <SearchBar onSearch={setSearchQuery} />
-            <OffersList offers={offers} /> */}
+            <OffersList offers={offers} />
         </div>
     );
 };
