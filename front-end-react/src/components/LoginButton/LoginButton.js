78956f091b0e1f6a2b73a6237aa4cd5017c12290
s24610 <s24610@pjwstk.edu.pl>
2024-11-09 17:03:17 +0100

    logout functionality

--- front-end-react/src/components/LoginButton/LoginButton.js
+++ front-end-react/src/components/LoginButton/LoginButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fetchLogout } from '../../services/LogOutService/LogOutService';
 
 const LoginButton = () => {
     const handleLogin = () => {
@@ -10,14 +11,27 @@ const LoginButton = () => {
         window.location.href = '/register';
     };
 
+    const handleLogOut = () => {
+        fetchLogout()
+        window.location.reload()
+    };
+
     return (
         <div>
-            <button onClick={handleLogin} style={{ position: 'absolute', top: 10, right: 10 }}>
-                Zaloguj
-            </button>
-            <button onClick={handleRegistration} style={{ position: 'absolute', top: 10, right: 70 }}>
-                Zarejestruj
-            </button>
+            {localStorage.getItem("jwt") === null ?
+            <div>
+                <button onClick={handleLogin} style={{ position: 'absolute', top: 10, right: 10 }}>
+                    Zaloguj
+                </button>
+                <button onClick={handleRegistration} style={{ position: 'absolute', top: 10, right: 70 }}>
+                    Zarejestruj
+                </button>
+            </div> : 
+            <div>
+                <button onClick={handleLogOut} style={{ position: 'absolute', top: 10, right: 10 }}>
+                    Wyloguj    
+                </button>    
+            </div>}
         </div>
     );
 };
---
+++ front-end-react/src/services/LogOutService/LogOutService.js
@@ -0,0 +1,11 @@
+export const fetchLogout = () => {
+    fetch('https://localhost:7166/api/User/logOut', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': `Bearer ${localStorage.getItem("jwt")}`
+        }
+    }).then(response => response.json())
+    localStorage.removeItem("jwt")
+}
