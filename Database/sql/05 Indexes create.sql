76573420c14eabc9fe2ce9cad9ff34439b78b93e
Artem Yumina <127307970+s25316@users.noreply.github.com>
2025-02-11 21:57:19 +0100

    2025-02-11 Update indexes

--- Database/sql/05 Indexes create.sql
+++ Database/sql/05 Indexes create.sql
@@ -1,3 +1,15 @@
 CREATE NONCLUSTERED INDEX IDX_AdministrativeDivision_ParentDivisionId
 ON [dbo].[AdministrativeDivision] (ParentDivisionId)
 INCLUDE (Id, Name, AdministrativeTypeId);
+
+CREATE NONCLUSTERED INDEX IDX_AdministrativeDivision_Name
+ON [dbo].[AdministrativeDivision] (Name)
+INCLUDE (Id, ParentDivisionId, AdministrativeTypeId);
+
+
+CREATE NONCLUSTERED INDEX IDX_Street_Name
+ON [dbo].[Street] (Name)
+INCLUDE (Id, AdministrativeTypeId);
+
+CREATE NONCLUSTERED INDEX IDX_DivisionStreet_Covered
+ON [dbo].[DivisionStreet] (DivisionId, StreetId);
--- Database/sql/05 Indexes delete.sql
+++ Database/sql/05 Indexes delete.sql
@@ -1,2 +1,11 @@
 DROP INDEX [dbo].[AdministrativeDivision].
 IDX_AdministrativeDivision_ParentDivisionId;
+
+DROP INDEX [dbo].[AdministrativeDivision].
+IDX_AdministrativeDivision_Name;
+
+DROP INDEX [dbo].[Street].
+IDX_Street_Name;
+
+DROP INDEX [dbo].[DivisionStreet].
+IDX_DivisionStreet_Covered;
